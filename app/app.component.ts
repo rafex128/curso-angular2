141f7e8f498d4766e88539df04f2ac3630691a03
Rafael <rafael128@hotmail.com>
2016-12-13 18:36:03 -0200

    Create first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { MeuPrimeiroComponent } from './primeiro/meu-primeiro.component';
+
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
-    `
+        <meu-primeiro-component></meu-primeiro-component>
+    `,
+    directives: [ MeuPrimeiroComponent ]
 })
 export class AppComponent { }
