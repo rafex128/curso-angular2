49626455e3888e2dfd45ef98422c4fbb0615233b
Rafael <rafael128@hotmail.com>
2016-12-13 19:44:23 -0200

    Added first component

---
+++ app/primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: '<h3>Iniciando com angular2<h3>'
+})
+
+export class MeuPrimeiroComponent {}
