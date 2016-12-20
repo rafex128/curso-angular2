26201c0542aafcc9367a7780b7c5137e205bd9aa
Rafael <rafael128@hotmail.com>
2016-12-19 23:08:41 -0200

    Added files

---
+++ app/data-binding/data-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'data-binding',
+  templateUrl: 'data-binding.component.html',
+})
+export class DataBindingComponent{
+  constructor() {  }
+}
